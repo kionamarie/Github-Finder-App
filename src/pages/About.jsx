acbcfd73ce5a47e38aed5922bc36f9ed67871ad8
Kiona Coats <kionacoats@gmail.com>
2023-01-04 20:24:29 -0500

    Added Routes. Linked About, Home, and NotFound pages.

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">GitHub Finder</h1>
+      <p className="mb-4 text-1xl">
+        A React app to search and view GitHub profiles.
+      </p>
+      <p>
+        This project is part of the <strong>React Front to Back 2022 </strong>
+        Udemy course by <strong> Brad Traversy.</strong>
+      </p>
+    </div>
+  );
+}
+
+export default About;
