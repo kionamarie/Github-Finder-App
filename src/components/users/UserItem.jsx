09b6d0bc35e59df40569c19c6edbfcff2475c950
Kiona <kiona.coats@us.af.mil>
2023-01-10 12:01:43 -0600

    Added Github context, provider, and reducer

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 function UserItem({ user: { login, avatar_url } }) {
   return (
     <div className='card shadow-md compact side bg-base-100'>
-      {login}
       <div className='flex-row items-center space-x-4 card-body'>
         <div>
           <div className='avatar'>
